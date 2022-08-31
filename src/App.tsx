40cd0ed272b5cf51075156a22cd0101751cba651
prima-nikita <prima.nikita.2022@gmail.com>
2022-08-31 23:28:20 +0600

    add colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "./components";
+import {colors} from "./constants";
 
 function App() {
   return (
@@ -7,6 +8,30 @@ function App() {
       <div style={{ margin: 25 }}>
         <Button title="Test button title" onClick={() => null} />
       </div>
+
+        colors.black
+      <div style={{ width: 250, height: 25, backgroundColor: colors.black }} />
+        colors.white
+      <div style={{ width: 250, height: 25, border: "1px solid black", backgroundColor: colors.white }} />
+        colors.pizazz
+      <div style={{ width: 250, height: 25, backgroundColor: colors.pizazz }} />
+        colors.silver
+      <div style={{ width: 250, height: 25, backgroundColor: colors.silver }} />
+       colors.turbo
+      <div style={{ width: 250, height: 25, backgroundColor: colors.turbo }} />
+       colors.broom
+      <div style={{ width: 250, height: 25, backgroundColor: colors.broom }} />
+       colors.alto
+      <div style={{ width: 250, height: 25, backgroundColor: colors.alto }} />
+       colors.vermilion
+      <div style={{ width: 250, height: 25, backgroundColor: colors.vermilion }} />
+       colors.internationalOrange
+      <div style={{ width: 250, height: 25, backgroundColor: colors.internationalOrange }} />
+       colors.greenPea
+      <div style={{ width: 250, height: 25, backgroundColor: colors.greenPea }} />
+        colors.monza
+      <div style={{ width: 250, height: 25, backgroundColor: colors.monza }} />
+
     </div>
   );
 }
--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,2 +1,11 @@
 export const black = '#000000';
 export const white = '#FFFFFF';
+export const pizazz = '#FD9100';
+export const silver  = '#C4C4C4';
+export const turbo = '#F9E000';
+export const broom = '#FFE927';
+export const alto = '#E0E0E0';
+export const vermilion = '#FC4710';
+export const internationalOrange = '#FE5313';
+export const greenPea = '#236E45';
+export const monza = '#D30011';
